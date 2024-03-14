fad01fc904ebe3b05ac15190be5963fa55d0cbf3
Luis Solier Sajami <luis.sajami@gmail.com>
2024-03-13 21:31:44 -0500

    Se agrega primera pantalla del flujo de MiDia.

---
+++ app/src/main/java/com/uniandes/miso/medicarealert/MiDia01Activity.kt
@@ -0,0 +1,27 @@
+package com.uniandes.miso.medicarealert
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.uniandes.miso.medicarealert.databinding.ActivityMiDia01Binding
+
+class MiDia01Activity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMiDia01Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMiDia01Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCompartirCodigo.setOnClickListener { loadMiDia02Activity(this ) }
+
+    }
+
+    private fun loadMiDia02Activity(context: Context) {
+        //val intent = Intent(context, MiDia02Activity::class.java)
+        //context.startActivity(intent)
+        //finish()
+    }
+}
