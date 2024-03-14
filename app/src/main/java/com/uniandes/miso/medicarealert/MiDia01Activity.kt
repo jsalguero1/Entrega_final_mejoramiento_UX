fe8bf718796d969dbbc623e1b374bdf1910cad96
Luis Solier Sajami <luis.sajami@gmail.com>
2024-03-13 21:33:41 -0500

    Se agrega segunda pantalla del flujo MiDia.

--- app/src/main/java/com/uniandes/miso/medicarealert/MiDia01Activity.kt
+++ app/src/main/java/com/uniandes/miso/medicarealert/MiDia01Activity.kt
@@ -1,6 +1,7 @@
 package com.uniandes.miso.medicarealert
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.uniandes.miso.medicarealert.databinding.ActivityMiDia01Binding
@@ -20,8 +21,8 @@ class MiDia01Activity: AppCompatActivity() {
     }
 
     private fun loadMiDia02Activity(context: Context) {
-        //val intent = Intent(context, MiDia02Activity::class.java)
-        //context.startActivity(intent)
-        //finish()
+        val intent = Intent(context, MiDia02Activity::class.java)
+        context.startActivity(intent)
+        finish()
     }
 }
---
+++ app/src/main/java/com/uniandes/miso/medicarealert/MiDia02Activity.kt
@@ -0,0 +1,28 @@
+package com.uniandes.miso.medicarealert
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.uniandes.miso.medicarealert.databinding.ActivityMiDia02Binding
+
+class MiDia02Activity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMiDia02Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMiDia02Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCompartirWsp.setOnClickListener { loadMiDia03Activity(this ) }
+
+    }
+
+    private fun loadMiDia03Activity(context: Context) {
+        //val intent = Intent(context, MiDia03Activity::class.java)
+        //context.startActivity(intent)
+        //finish()
+    }
+
+}
